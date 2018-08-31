d760dfd3985772af07c6ea2dbae9cb370b318c5c
刘春牢 <liuchunlao@qq.com>
2018-08-31 19:04:50 +0800

    刷新

--- Baisi/Essense/Func/BAIWordController.m
+++ Baisi/Essense/Func/BAIWordController.m
@@ -20,9 +20,25 @@ static NSString *cellid = @"topic";
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self.tableView registerNib:[UINib nibWithNibName:@"BAITopicCell" bundle:nil] forCellReuseIdentifier:cellid];
+    [self setupUI];
+    [self setupRefresh];
+    
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+}
+
+#pragma mark - 数据加载
+- (void)loadTopics {
+    [self.tableView.mj_header endRefreshing];
+}
+
+- (void)loadMoreTopics {
+    
+    [self.tableView.mj_footer endRefreshing];
+}
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return 200;
@@ -39,6 +55,24 @@ static NSString *cellid = @"topic";
     
     
     return cell;
+}
+
+#pragma mark - 搭建界面
+- (void)setupUI {
+ 
+    [self.tableView registerNib:[UINib nibWithNibName:@"BAITopicCell" bundle:nil] forCellReuseIdentifier:cellid];
+}
+
+- (void)setupRefresh {
+    self.tableView.mj_header = [MJRefreshNormalHeader headerWithRefreshingTarget:self refreshingAction:@selector(loadTopics)];
+    self.tableView.mj_header.automaticallyChangeAlpha = YES;
+    
+    // 不加延时tableView会向上偏移!!!
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self.tableView.mj_header beginRefreshing];
+    });
+    
+    self.tableView.mj_footer = [MJRefreshAutoFooter footerWithRefreshingTarget:self refreshingAction:@selector(loadMoreTopics)];
 }
 
 
